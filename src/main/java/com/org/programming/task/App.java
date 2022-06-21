3f85fcb79c4c75a83c9c42f6e3a0790ac4de1b8e
Sivakumar Amirthalingam <vanaja.sivakumar@hotmail.com>
2022-06-21 16:50:03 +1000

    removing unused methods

--- src/main/java/com/org/programming/task/App.java
+++ src/main/java/com/org/programming/task/App.java
@@ -68,36 +68,6 @@ public class App {
 
 	}
 	
-	private static LinkedHashMap<String, Integer> sortMapByValue(Map<String, Integer> map) {
-
-		Comparator<Entry<String, Integer>> valueComparator = new Comparator<Entry<String, Integer>>() {
-
-			@Override
-			public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2) {
-				Integer v1 = e1.getValue();
-				Integer v2 = e2.getValue();
-				return v2.compareTo(v1);
-			}
-		};
-
-		// Sort method needs a List, so let's first convert Set to List in Java
-		Set<Entry<String, Integer>> entries = map.entrySet();
-		List<Entry<String, Integer>> listOfEntries = new ArrayList<Entry<String, Integer>>(entries);
-
-		// sorting HashMap by values using comparator
-		Collections.sort(listOfEntries, valueComparator);
-
-		LinkedHashMap<String, Integer> sortedByValue = new LinkedHashMap<String, Integer>(listOfEntries.size());
-
-		// copying entries from List to Map
-		for (Entry<String, Integer> entry : listOfEntries) {
-			sortedByValue.put(entry.getKey(), entry.getValue());
-		}
-
-		return sortedByValue;
-	}
-	
-	
 	private static List<AuditLog> parseLogFile() {
 		File logFile = new File("src/main/resources/programming-task-example-data.txt");
 		List<AuditLog> auditLogList = new ArrayList<AuditLog>();
--- src/main/java/com/org/programming/task/service/URLVisitLogService.java
+++
@@ -1,8 +0,0 @@
-package com.org.programming.task.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class URLVisitLogService {
-
-}
